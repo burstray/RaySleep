fd2c89269df8d0e0a56bb0ba6b52479c6daa62ca
burstray <burstray@gmail.com>
2021-06-05 16:13:40 +0800

    Init

---
+++ RaySleep/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RaySleep
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Thread.Sleep(10000);
+            Application.SetSuspendState(PowerState.Suspend, true, true);
+        }
+    }
+}
